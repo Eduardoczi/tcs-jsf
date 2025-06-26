d4493e778f1e53a4d1c7c12c6477c6a83694d8e4
h0wzy <h0wzymarcos@gmail.com>
2025-06-26 14:33:52 -0300

    Subindo novo

--- src/main/java/br/com/tcs/treinamento/model/EmpresaVO.java
+++ src/main/java/br/com/tcs/treinamento/model/EmpresaVO.java
@@ -1,63 +1,33 @@
-package br.com.tcs.treinamento.entity;
+package br.com.tcs.treinamento.model;
 
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
-import javax.persistence.*;
 
-@Entity
-@Table(name = "Empresa")
-public class Empresa implements Serializable {
-
+public class EmpresaVO implements Serializable {
     private static final long serialVersionUID = 1L;
-
-    // Identificador da entidade
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    // Nome da pessoa (obrigatório)
-    @Column(nullable = false)
     private String nome;
-
-    // E-mail (obrigatório)
-    @Column(nullable = false)
     private String email;
-
-    // Data de fundação
-    @Temporal(TemporalType.DATE)
     private Date data;
-
-    // Número do CNPJ (caso tipoDocumento seja CNPJ)
     private String numeroCNPJ;
-
-    // Motivo da manutenção (alteração, exclusão ou desativação)
     private String motivoManutencao;
-
-    // Data da última manutenção (grava data e hora)
-    @Temporal(TemporalType.TIMESTAMP)
     private Date dataManutencao;
-
-    // Flag para exclusão lógica (soft delete)
     private Boolean ativo = true;
 
-    // Construtor padrão
-    public Empresa() {
-    }
-
-    // Construtor completo (opcional para facilitar a criação)
-    public Empresa(String nome, String email, Date data,
-                  String numeroCNPJ, String motivoManutencao, Date dataManutencao, Boolean ativo) {
+    public EmpresaVO(Long id,String nome,String email, Date data, String numeroCNPJ, Date dataManutencao, String motivoManutencao, Boolean ativo) {
+        this.id = id;
         this.nome = nome;
         this.email = email;
         this.data = data;
         this.numeroCNPJ = numeroCNPJ;
-        this.motivoManutencao = motivoManutencao;
         this.dataManutencao = dataManutencao;
+        this.motivoManutencao = motivoManutencao;
         this.ativo = ativo;
     }
 
-    // Getters e Setters
+    public EmpresaVO() {
+    }
 
     public Long getId() {
         return id;
@@ -123,35 +93,15 @@ public class Empresa implements Serializable {
         this.ativo = ativo;
     }
 
-
-    // equals e hashCode baseados no id (único identificador)
-
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Empresa empresa = (Empresa) o;
-        return Objects.equals(id, empresa.id);
+        EmpresaVO empresaVO = (EmpresaVO) o;
+        return Objects.equals(id, empresaVO.id) && Objects.equals(nome, empresaVO.nome) && Objects.equals(email, empresaVO.email) && Objects.equals(data, empresaVO.data)&& Objects.equals(numeroCNPJ, empresaVO.numeroCNPJ) && Objects.equals(motivoManutencao, empresaVO.motivoManutencao) && Objects.equals(dataManutencao, empresaVO.dataManutencao) && Objects.equals(ativo, empresaVO.ativo);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    // Método toString opcional para facilitar a depuração
-
-    @Override
-    public String toString() {
-        return "Pessoa{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                ", email='" + email + '\'' +
-                ", data=" + data +
-                ", numeroCNPJ='" + numeroCNPJ + '\'' +
-                ", motivoManutencao='" + motivoManutencao + '\'' +
-                ", dataManutencao=" + dataManutencao +
-                ", ativo=" + ativo +
-                '}';
+        return Objects.hash(id, nome, email, data, numeroCNPJ, motivoManutencao, dataManutencao, ativo);
     }
 }
